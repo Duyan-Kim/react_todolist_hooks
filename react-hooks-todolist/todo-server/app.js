67a10d794149c5bbdf77b32ffc51511c741b697e
Duyan-Kim <arbolkim@gmail.com>
2021-01-02 23:15:44 +0900

    make todo server

---
+++ react-hooks-todolist/todo-server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+const todos = [
+  {
+    id: 1,
+    title: "쿠키 굽기",
+  },
+  {
+    id: 2,
+    title: "앞구르기",
+  },
+  {
+    id: 3,
+    title: "번지점프",
+  },
+];
+
+app.use(cors());
+app.options("*", cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/todo", (req, res) => res.json(todos));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
