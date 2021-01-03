b25dd8b578177e29d5d96f4cc852d82bc2e8d41a
Duyan-Kim <arbolkim@gmail.com>
2021-01-03 22:47:16 +0900

    App.js

--- react-hooks-todolist/src/App.js
+++ react-hooks-todolist/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import List from "./List.jsx";
+import useFetch from "./useFetch.js";
+import Header from "./Header.jsx";
+import Form from "./Form.jsx";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState();
+  const loading = useFetch(setTodos, "http://localhost:8080/todo");
+
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, title: newTodo, status: "todo" },
+    ]);
+  };
+
+  const changeTodoStatus = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === +id) {
+        if (todo.status === "done") todo.status = "todo";
+        else todo.status = "done";
+      }
+      return todo;
+    });
+    setTodos(updateTodos);
+  };
+
+  useEffect(() => {
+    console.log("렌더링했어요", todos);
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header todos={todos} />
+      <Form addTodo={addTodo} changeTodoStatus={changeInputData} />
+      <List
+        todos={todos}
+        loading={loading}
+        changeTodoStatus={changeTodoStatus}
+      />
+    </>
   );
-}
+};
 
 export default App;
